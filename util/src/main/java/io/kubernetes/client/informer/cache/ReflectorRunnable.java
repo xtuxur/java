abbb9a5a8df0d6fd61054abdb33597f5e91a2fd4
hectorad <alex.delzar@gmail.com>
2020-11-30 08:58:21 -0600

    added extra logging

--- util/src/main/java/io/kubernetes/client/informer/cache/ReflectorRunnable.java
+++ util/src/main/java/io/kubernetes/client/informer/cache/ReflectorRunnable.java
@@ -107,6 +107,8 @@ public class ReflectorRunnable<
                       Long.valueOf(Duration.ofMinutes(2).toMillis()).intValue()));
           watchHandler(watch);
         } catch (Throwable t) {
+          log.debug("Exception happened but not isConnectException(t)");
+          log.debug("message:{} cause:{}",t.getMessage(),t.getCause());
           if (isConnectException(t)) {
             // If this is "connection refused" error, it means that most likely
             // apiserver is not
